63afab80a7cfb6fc11970216888834577e4008f4
DuyKhanh3112 <voduykhanh3112@gmail.com>
2024-12-25 14:42:41 +0700

    update 25/12

--- client/src/pages/sign/SignDocumentAway.tsx
+++ client/src/pages/sign/SignDocumentAway.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ISignDocument } from "interfaces";
 import { RootState } from "stores/reducers";
 import { useSelector } from "react-redux";
--- client/src/pages/sign/SignDocumentDetail.tsx
+++ client/src/pages/sign/SignDocumentDetail.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 
 import { RootState } from "stores/reducers";
 import { useSelector } from "react-redux";
